13b0a6fb94db42ba77a6bb9072821b814d63a006
Dejen Tesfai <107037555+Deju35@users.noreply.github.com>
2022-11-10 23:14:40 +0200

    Update model.py

--- model.py
+++ model.py
@@ -17,3 +17,22 @@ im.to_thumb(256,256)
 
 download_url(search_images('snake photos', max_images=1)[0], 'snake.jpg', show_progress=False)
 Image.open('snake.jpg').to_thumb(256,256)
+
+
+searches = 'cat','snake'
+path = Path('cat_or_not')
+from time import sleep
+
+for o in searches:
+    dest = (path/o)
+    dest.mkdir(exist_ok=True, parents=True)
+    download_images(dest, urls=search_images(f'{o} photo'))
+    sleep(10)  # Pause between searches to avoid over-loading server
+    download_images(dest, urls=search_images(f'{o} sun photo'))
+    sleep(10)
+    download_images(dest, urls=search_images(f'{o} shade photo'))
+    sleep(10)
+    resize_images(path/o, max_size=400, dest=path/o)
+failed = verify_images(get_image_files(path))
+failed.map(Path.unlink)
+len(failed)
