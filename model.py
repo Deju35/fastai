5d684acc30c032cd6a5b8c48c242296df5f4df6d
Dejen Tesfai <107037555+Deju35@users.noreply.github.com>
2022-11-10 23:15:11 +0200

    Update model.py

--- model.py
+++ model.py
@@ -36,3 +36,13 @@ for o in searches:
 failed = verify_images(get_image_files(path))
 failed.map(Path.unlink)
 len(failed)
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path, bs=32)
+
+dls.show_batch(max_n=10)
